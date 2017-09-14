ffec6b04d3a11502c894ac6e9840d54ffbda549e
V d M <vhmoura@gmail.com>
2017-09-14 15:07:32 -0300

    added private routes

--- server/server.js
+++ server/server.js
@@ -14,10 +14,11 @@ var port = process.env.PORT || 3000;
 var app = express();
 app.use(bodyparser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
    console.log(req.body);
    var todo = new Todo({
-       text: req.body.text
+       text: req.body.text,
+       _creator: req.user._id
    });
 
    todo.save().then((doc)=> {
@@ -27,8 +28,8 @@ app.post('/todos', (req, res) => {
    })
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({_creator: req.user._id}).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
